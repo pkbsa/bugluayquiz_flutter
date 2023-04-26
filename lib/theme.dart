738490ccae109d8a8e40dc5764b1b76f4d889b98
pkbsa <siranut.poko@hotmail.com>
2023-04-26 11:07:13 +0700

    remove google font

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 const bugluayColor = Color.fromRGBO(62, 39, 123, 1);
 
@@ -8,8 +8,5 @@ ThemeData get theme {
     scaffoldBackgroundColor: bugluayColor,
     useMaterial3: true,
   );
-  return theme.copyWith(
-    textTheme: GoogleFonts.kanitTextTheme(theme.textTheme),
-    primaryTextTheme: GoogleFonts.kanitTextTheme(theme.primaryTextTheme),
-  );
+  return theme;
 }
